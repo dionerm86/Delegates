11e9af209d998a87cfbf51ea157513de7c2c1634
Dione Regis Maciel <dionerm86@gmail.com>
2019-06-22 14:15:46 -0300

    Exemplo de Delegate

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -2,11 +2,29 @@
 
 namespace Delegates
 {
+    delegate double BinaryNumericOperation(double n1, double n2);
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation opMax = CalculationService.Sum;
+            double resultMax = opMax(a, b);
+            Console.WriteLine("Max: " + resultMax);
+
+            BinaryNumericOperation opSum = CalculationService.Sum;
+            double resultSum = opSum(a, b);
+            Console.WriteLine("Sum: " + resultSum);
+
+            //Com o método Square não funcionar pq a assinatura do delegate é diferente da assinatura do próprio Square. Qtde de parametros diferentes
+            //BinaryNumericOperation opSquare = CalculationService.Square;
+            //double resultSquare = opSquare(a);
+            //Console.WriteLine("Square: " + resultSquare);
+
+            Console.ReadLine();
         }
     }
 }
